5d05b9afc51043527bd43256836ed19b02077891
iliyan <iliyan@phyreapp.com>
2016-11-13 03:25:15 +0200

    Ready for submission tasks comment updated

--- up_homework_1/homework_1_tests/fn81521_prob6_gcc.cpp
+++ up_homework_1/homework_1_tests/fn81521_prob6_gcc.cpp
@@ -1,16 +1,16 @@
 /**
-*  
-* Solution to homework task 
-* Introduction to programming course 
-* Faculty of Mathematics and Informatics of Sofia University 
-* Winter semester 2016/2017 
-* 
-* @author Iliyan Germanov 
-* @idnumber 81521 
-* @task 6 
-* @compiler GCC 
-* 
-*/
+ *  
+ * Solution to homework task 
+ * Introduction to programming course 
+ * Faculty of Mathematics and Informatics of Sofia University 
+ * Winter semester 2016/2017 
+ * 
+ * @author Iliyan Germanov 
+ * @idnumber 81521 
+ * @task 6 
+ * @compiler GCC 
+ * 
+ */
 #include <iostream>
 #include <algorithm>
 
--- up_homework_1/ready_for_submission/fn81521_prob1_gcc.cpp
+++ up_homework_1/ready_for_submission/fn81521_prob1_gcc.cpp
@@ -1,16 +1,16 @@
 /**
-*  
-* Solution to homework task 
-* Introduction to programming course 
-* Faculty of Mathematics and Informatics of Sofia University 
-* Winter semester 2016/2017 
-* 
-* @author Iliyan Germanov 
-* @idnumber 81521 
-* @task 1 
-* @compiler GCC 
-* 
-*/
+ *  
+ * Solution to homework task 
+ * Introduction to programming course 
+ * Faculty of Mathematics and Informatics of Sofia University 
+ * Winter semester 2016/2017 
+ * 
+ * @author Iliyan Germanov 
+ * @idnumber 81521 
+ * @task 1 
+ * @compiler GCC 
+ * 
+ */
 /*
  * Warning: Java and Android conventions used!
  */
