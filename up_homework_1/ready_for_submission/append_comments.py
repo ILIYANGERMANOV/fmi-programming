9c226307e9e36a17fcfad115888806ed61dbe90a
iliyan <iliyan@phyreapp.com>
2016-11-13 03:19:37 +0200

    Documentation comment to append format updated

--- up_homework_1/ready_for_submission/append_comments.py
+++ up_homework_1/ready_for_submission/append_comments.py
@@ -2,7 +2,7 @@ import sys, os, re
 FILE_PATH = sys.argv[1]
 TASK_NUMBER = sys.argv[2]
 
-COMMENT = "/**\n*  \n* Solution to homework task \n* Introduction to programming course \n* Faculty of Mathematics and Informatics of Sofia University \n* Winter semester 2016/2017 \n* \n* @author %s \n* @idnumber %s \n* @task %s \n* @compiler %s \n* \n*/"
+COMMENT = "/**\n *  \n * Solution to homework task \n * Introduction to programming course \n * Faculty of Mathematics and Informatics of Sofia University \n * Winter semester 2016/2017 \n * \n * @author %s \n * @idnumber %s \n * @task %s \n * @compiler %s \n * \n */"
 
 NAME = "Iliyan Germanov"
 FACULTY_NUMBER = "81521"
