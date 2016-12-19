7c87623969bae6d83e1b3487a749c7aac654c96a
iliyan <iliyan@phyreapp.com>
2016-12-19 12:15:46 +0200

    Testing and preparing for submission task_7

--- up_homework_2/ready_for_submission/main.cpp
+++ up_homework_2/ready_for_submission/main.cpp
@@ -7,66 +7,67 @@
  * 
  * @author Iliyan Germanov 
  * @idnumber 81521 
- * @task 2 
+ * @task 7 
  * @compiler GCC 
  * 
  */
 #include <iostream>
-#include <stdexcept>
-#include <string>
-#include <sstream>
-#include <cmath>
 
 using namespace std;
 
-#define MAX_N_VALUE 100000
+//TODO: Fix issues so it can work properly
 
-class TwinPrimesGenerator {
-    int mN;
-public:
-    TwinPrimesGenerator(int n) {
-        if (n <= 0 || n > MAX_N_VALUE) {
-            stringstream sStream;
-            sStream << "N must be in (0, " << MAX_N_VALUE << ')';
-            throw invalid_argument(sStream.str());
-        }
-        mN = n;
-    }
+#define MAX_INPUT_VALUE 1000001
 
-    void generateAndPrint() {
-        cout << "3 5" << endl;
-        int middleHolder, counter = 1;
-        int printedTwinPrimes = 1, firstTwin, secondTwin;
-        while (printedTwinPrimes < mN) {
-            middleHolder = 6 * counter;
-            firstTwin = middleHolder - 1;
-            secondTwin = middleHolder + 1;
-            if (isPrimeNumber(firstTwin) && isPrimeNumber(secondTwin)) {
-                cout << firstTwin << ' ' << secondTwin << endl;
-                printedTwinPrimes++;
-            }
-            counter++;
-        }
-    }
+bool validateInput(int n, int m, int y) {
+    if (n <= 0 || n >= MAX_INPUT_VALUE) return false;
+    if (m <= 1 || m >= MAX_INPUT_VALUE) return false;
+    if (y <= 0 || y >= m) return false;
+    return true;
+}
 
-private:
-    bool isPrimeNumber(int number) {
-        int largesPossibleDivider = (int) sqrt(number);
-        for (int divider = 2; divider <= largesPossibleDivider; ++divider) {
-            if (number % divider == 0) return false;
-        }
-        return true;
+int calculateNumber(int a, int n, int m) {
+    int result = 1;
+    while (n > 0) {
+        result *= a;
+        result %= m;
+        --n;
     }
-};
+    return result % m;
+}
+
+
+void printSpaces(bool &isFirstPrinted) {
+    if (isFirstPrinted) {
+        isFirstPrinted = false;
+    } else {
+        cout << ' ';
+    }
+}
 
 int main() {
-    int n;
+    int n, m, y;
     cin >> n;
-    try {
-        TwinPrimesGenerator twinPrimesGenerator = TwinPrimesGenerator(n);
-        twinPrimesGenerator.generateAndPrint();
-    } catch (invalid_argument e) {
-        cout << e.what() << endl;
+    cin >> m;
+    cin >> y;
+    if (!validateInput(n, m, y)) {
+        cout << "Invalid input!" << endl;
+        return -1;
     }
+    int calculatedNumber;
+    bool isFirstPrint = true;
+    bool isAnyNumberPrinted = false;
+    for (int x = 1; x <= m - 1; ++x) {
+        calculatedNumber = calculateNumber(x, n, m);
+        if (calculatedNumber == y) {
+            printSpaces(isFirstPrint);
+            cout << x;
+            isAnyNumberPrinted = true;
+        }
+    }
+    if (!isAnyNumberPrinted) {
+        cout << "-1";
+    }
+    cout << endl;
     return 0;
 }
