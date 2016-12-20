19cf59b4fa2b75c7cc05e55bb7a0ba139753f123
iliyan <iliyan@phyreapp.com>
2016-12-20 23:56:57 +0200

    Solving task_9 (remove digit to receive a number)

--- up_homework_2/task_9/main.cpp
+++ up_homework_2/task_9/main.cpp
@@ -2,25 +2,42 @@
 
 using namespace std;
 
-//123 1
-
-
-
 int getNumberLength(int number) {
     int length = 1;
     while ((number /= 10) != 0) length++;
     return length;
 }
 
-bool canBeTransformedByRemovingOneDigit(int firstNumber, int secondNumber) {
-    int firstNumLen = getNumberLength(firstNumber);
-    int secondNumLen = getNumberLength(secondNumber);
+int getValueWithoutDigit(int number, int digitIndex) {
+    int result = 0;
+    int base = 1;
+    for (int i = 0; i <= digitIndex; ++i, number /= 10) {
+        if (i == digitIndex) {
+            number /= 10;
+            break;
+        }
+        result += (number % 10) * base;
+        base *= 10;
+    }
+    return result + number * base;
+}
+
+bool bruteForce(unsigned int firstNum, int firstNumLen, unsigned int secondNum) {
+    for (int i = 0; i < firstNumLen; ++i) {
+        if (getValueWithoutDigit(firstNum, i) == secondNum) return true;
+    }
+    return false;
+}
+
+bool canBeTransformedByRemovingOneDigit(unsigned int firstNum, unsigned int secondNum) {
+    int firstNumLen = getNumberLength(firstNum);
+    int secondNumLen = getNumberLength(secondNum);
     if (firstNumLen <= secondNumLen || firstNumLen > secondNumLen + 1) return false;
-    return true;
+    return bruteForce(firstNum, firstNumLen, secondNum);
 }
 
 int main() {
-    int firstNumber, secondNumber;
+    unsigned int firstNumber, secondNumber;
     cin >> firstNumber;
     cin >> secondNumber;
     if (canBeTransformedByRemovingOneDigit(firstNumber, secondNumber)) {
